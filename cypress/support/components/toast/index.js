1de38ed042f615cdc9aff0c8850d83621b1c826e
Fernanda Mendes Rodrigues <nandah.msi@gmail.com>
2022-03-20 18:14:02 -0300

    projeto samuraibs

---
+++ cypress/support/components/toast/index.js
@@ -0,0 +1,12 @@
+import { el } from './elements'
+
+class Toast {
+    shouldHaveTest(expectText) {
+        cy.get(el.toast)
+            .should('be.visible')
+            .find('p')
+            .should('have.text', expectText)
+    }
+}
+
+export default new Toast()

483578e95a2c8023b1a8491e0791b039267f97cc
Fernanda <fernanda.rodrigues@db1.com.br>
2022-04-11 22:07:42 -0300

    encerrando o modulo Consumindo a API REST

---
+++ cypress/integration/dash.spec.js
@@ -0,0 +1,49 @@
+
+import loginPage from '../support/pages/login'
+import dashPage from '../support/pages/dash'
+
+describe('dashboard', function () {
+
+    context('quando o cliente faz um agendamento no app mobile', function () {
+
+        const data = {
+            customer: {
+                name: 'Nikki Sixx',
+                email: 'sixx@motleycrue.com',
+                password: 'pwd123',
+                is_provider: false
+            },
+
+            appointmentHour: '14:00',
+
+            provider: {
+                name: 'Ramon Valdes',
+                email: 'ramon@televisa.com',
+                password: 'pwd123',
+                is_provider: true
+            }
+        }
+
+        before(function () {
+            cy.postUser(data.provider)
+            cy.postUser(data.customer)
+
+            cy.apiLogin(data.customer)
+            cy.setProviderId(data.provider.email)
+            cy.createAppointment(data.appointmentHour)
+        })
+
+        it('o mesmo deve ser exibido no dashboard', function () {
+            loginPage.go()
+            loginPage.form(data.provider)
+            loginPage.submit()
+
+            dashPage.calendarShouldBeVisible()
+
+            const day = Cypress.env('appointmentDay')
+            dashPage.selectDay(day)
+
+            dashPage.appointmentShouldBe(data.customer, data.appointmentHour)
+        })
+    })
+})
